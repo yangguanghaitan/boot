17dbc0e191ddb63ef485102f799b91dcfb9e0f5f
yangguanghaitan <2898666969@qq.com>
2022-02-16 11:44:31 +0800

    spring中bean的生命周期+1

---
+++ src/main/java/com/bootlearn/boot/lifeCycle/BeanLifeCycle.java
@@ -0,0 +1,34 @@
+package com.bootlearn.boot.lifeCycle;
+
+
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author d
+ * @date 2022/2/16 10:52
+ * @Describe   spring bean的生命周期
+ *             {@link BeanFactoryPostProcessor}:插手bean工厂的容器启动最后阶段,当有多个时，实现
+ *             {@link org.springframework.core.Ordered},指定bean工厂后置处理器的执行顺序，常用
+ *             的实现类 {@link org.springframework.context.support.PropertySourcesPlaceholderConfigurer}和
+ *             {@link org.springframework.beans.factory.config.PropertyOverrideConfigurer}和
+ *             {@link org.springframework.beans.factory.config.CustomEditorConfigurer};
+ *
+ **/
+public class BeanLifeCycle {
+
+    public static void main(String[] args) {
+
+        System.out.println("现在开始初始化容器");
+
+        ApplicationContext factory = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("容器初始化成功");
+        //得到Preson，并使用
+        Person person =   factory.getBean("person", Person.class);
+        System.out.println(person);
+
+        System.out.println("现在开始关闭容器！");
+        ((ClassPathXmlApplicationContext)factory).registerShutdownHook();
+    }
+}
---
+++ src/main/java/com/bootlearn/boot/lifeCycle/MyInstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,49 @@
+package com.bootlearn.boot.lifeCycle;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
+
+import java.beans.PropertyDescriptor;
+
+public class MyInstantiationAwareBeanPostProcessor extends
+        InstantiationAwareBeanPostProcessorAdapter {
+
+    public MyInstantiationAwareBeanPostProcessor() {
+        super();
+//        System.out.println("这是InstantiationAwareBeanPostProcessorAdapter实现类构造器！！");
+    }
+
+    // 接口方法、实例化 Bean之前调用
+    @Override
+    public Object postProcessBeforeInstantiation(Class beanClass,
+                                                 String beanName) throws BeansException {
+        System.out.println("InstantiationAwareBeanPostProcessor调用postProcessBeforeInstantiation方法");
+        return null;
+    }
+
+    @Override
+    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+
+        System.out.println("InstantiationAwareBeanPostProcessor调用postProcessAfterInstantiation方法");
+        return true;
+    }
+
+    // 接口方法、初始化 Bean之后调用
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+        System.out.println("InstantiationAwareBeanPostProcessor调用postProcessAfterInitialization方法");
+        return bean;
+    }
+
+    // 接口方法、设置某个属性时调用
+    @Override
+    public PropertyValues postProcessPropertyValues(PropertyValues pvs,
+                                                    PropertyDescriptor[] pds, Object bean, String beanName)
+            throws BeansException {
+//        System.out.println("InstantiationAwareBeanPostProcessor调用postProcessPropertyValues方法");
+        return pvs;
+    }
+}
