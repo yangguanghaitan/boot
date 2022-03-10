1ebcd0cb0966fbdedcfe25b101dcf8240af7cdac
yangguanghaitan <2898666969@qq.com>
2022-03-10 12:15:27 +0800

    thymeleaf

---
+++ src/main/java/com/bootlearn/boot/thymeleaf/TestThymeleafController.java
@@ -0,0 +1,22 @@
+package com.bootlearn.boot.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Auther d
+ * @Date 2022/3/10 11:52
+ * @Describe
+ **/
+@Controller
+public class TestThymeleafController {
+
+    @GetMapping("/thyme")
+    public String getThymeleaf(Long id,Map map) {
+        map.put("aa","dddd");
+        return "thyme";
+    }
+}
